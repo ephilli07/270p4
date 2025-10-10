2b36ef8afc1cfbdfc5745e5c01504d93e7c620cb
Phillips <ephilli@UMICH.EDU>
2025-10-10 12:32:03 -0400

    writing tests

--- TestBench4.v
+++ TestBench4.v
@@ -7,9 +7,6 @@ module TestBench4();
 	wire [6:0] HEX5, HEX4;   // B
 	wire [6:0] HEX3, HEX2;   // R
 	wire [6:0] HEX0;         // OVF: Display E (Error) on Overflow
-	
-	wire signed [3:0] R;
-	wire ovf;
 
 // Instantiate the calculator and number converters
 Project4 calculator(
@@ -21,36 +18,31 @@ Project4 calculator(
 	.HEX4(HEX4), 
 	.HEX3(HEX3), 
 	.HEX2(HEX2), 
-	.HEX0(HEX0),
+	.HEX0(HEX0)
 );
 
 
+// PROJECT 4 TEST GLOSSARY
+// Welcome to my comprehensive test glossary. 
+// Each test is specified below. 
+// -------------------------------------------
+// TEST 1 A + B = POSITIVE, NO OVERFLOW (1 + 3 = 4)
+// Expected: 
+// -------------------------------------------
+// TEST 2 A - B = POSITIVE, NO OVERFLOW 
+// Expected: 
+// -------------------------------------------
+// TEST A - B = NEGATIVE, 
+// Expected: 
+// -------------------------------------------
+// TEST  ABS(-8), OVERFLOW
+// Expected: 
+// -------------------------------------------
 
-// Specify your test procedures
 initial begin
-	// Test 1: A(0) + B(1) = R(1)
-	KEY <= 3'b000; #10;		
-	SW <= 8'b00000000; #10;
-    SW <= 8'b00000001; #10;
-	// show A, B, Overflow, and R on HEX displays.
-
-	// Test 2: No overflow add (negative + negative = positive)
-
-	// Test 3: Overflow add (positive + positive = negative)
-
-	// Test 4: No overflow subtract (negative minus positive = positive)
-
-	// Test 5: Overflow subtract (positive minus negative = negative)
-
-	// Test 6: Overflow of bit postions (7 + 1)
-
-	// Test 7: Regular add 
-
-	// Test 8: Regular subtract
-
-	// Test 9: Absolute value of postive, remains positive
-
-	// Test 10: Absolute value of negative, becomes positive
+// TEST 1: 
+KEY = 3'b000; 
+SW = 
 
 end
 
