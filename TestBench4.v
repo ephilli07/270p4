38bf02260c7bd89f3e61c66094b08b61732e74f9
Phillips <ephilli@UMICH.EDU>
2025-10-10 12:39:10 -0400

    add test cases

--- TestBench4.v
+++ TestBench4.v
@@ -21,28 +21,42 @@ Project4 calculator(
 	.HEX0(HEX0)
 );
 
-
+// ====================================================
 // PROJECT 4 TEST GLOSSARY
 // Welcome to my comprehensive test glossary. 
 // Each test is specified below. 
+
+// ADD TESTS
 // -------------------------------------------
 // TEST 1 A + B = POSITIVE, NO OVERFLOW (1 + 3 = 4)
-// Expected: 
+// Expected: HEX7/HEX6 1 HEX5/HEX4 3 HEX3/HEX2 4 HEX0 off
 // -------------------------------------------
-// TEST 2 A - B = POSITIVE, NO OVERFLOW 
-// Expected: 
+// TEST 2 A + B = POSITIVE, OVERFLOW (7 + 1 = 8) 
+// Expected: HEX7/HEX6 7 HEX5/HEX4 1 HEX3/HEX2 8 HEX0 on
 // -------------------------------------------
-// TEST A - B = NEGATIVE, 
-// Expected: 
+// TEST 3 -A + -B = NEGATIVE, OVERFLOW (-5 + -12 = -17) 
+// Expected: HEX7/HEX6 7 HEX5/HEX4 1 HEX3/HEX2 8 HEX0 on
 // -------------------------------------------
-// TEST  ABS(-8), OVERFLOW
-// Expected: 
+// TEST 4 A + -B = POSITIVE, OVERFLOW (-5 + -12 = -17) 
+// Expected: HEX7/HEX6 7 HEX5/HEX4 1 HEX3/HEX2 8 HEX0 on
 // -------------------------------------------
+// TEST 5 -A + -B = 0, NO OVERFLOW (-7 + 7 = 0) 
+// Expected: HEX7/HEX6 -7 HEX5/HEX4 7 HEX3/HEX2 0 HEX0 off
+
+
+
+// ====================================================
+
 
 initial begin
-// TEST 1: 
+
+// TEST 1 
 KEY = 3'b000; 
-SW = 
+SW = 8'b00010011; 
+#10; 
+
+// TEST 2
+
 
 end
 
