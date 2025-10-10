4ecccae266318ccae5d7e8f2fcba9b90165ffd42
Phillips <ephilli@UMICH.EDU>
2025-10-10 10:39:57 -0400

    testing

--- TestBenchFullAdder.v
+++ TestBenchFullAdder.v
@@ -1,11 +1,20 @@
 `timescale 1ns/1ns
 
 module TestBenchFullAdder( 
-
+    
 );
 
 
-FullAdder testFullAdder(); 
+// Need place to have sum and carry out
+wire sum, carryOut; 
+
+FullAdder testFullAdder(
+    .a(), 
+    .b(),
+    .cin(), 
+    .s(sum), 
+    .cout(carryOut)
+); 
 
 initial begin 
     
